eba0f7d7f4de75116e5b75c1a6c457cc1a9083de
davisbd100 <davisbd100@gmail.com>
2020-04-09 22:37:33 -0500

    Creacion de primer Interfaz

---
+++ Repositories/IArticuloRepository.cs
@@ -0,0 +1,14 @@
+using MiPrimerApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiPrimerApi.Repositories
+{
+    public interface IArticuloRepository
+    {
+        List<Articulo> ObtenerTodos();
+        Articulo ObtenerPorId(int id);
+    }
+}
