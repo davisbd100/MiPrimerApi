cc1fd4228894c1933841f95c1f2c6b0071cf5ccf
davisbd100 <davisbd100@gmail.com>
2020-04-09 22:18:19 -0500

    Migration de relacion

---
+++ Migrations/20200410031720_RelacionArticuloProveedor.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MiPrimerApi.Migrations
+{
+    public partial class RelacionArticuloProveedor : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ProveedorId",
+                table: "Articulos",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Articulos_ProveedorId",
+                table: "Articulos",
+                column: "ProveedorId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Articulos_Proveedores_ProveedorId",
+                table: "Articulos",
+                column: "ProveedorId",
+                principalTable: "Proveedores",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Articulos_Proveedores_ProveedorId",
+                table: "Articulos");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Articulos_ProveedorId",
+                table: "Articulos");
+
+            migrationBuilder.DropColumn(
+                name: "ProveedorId",
+                table: "Articulos");
+        }
+    }
+}
